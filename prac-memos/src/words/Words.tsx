469cf50c1c515a0753a56386fae6c4cb6c65988a
taehyeong park <b_tae96@naver.com>
2022-07-07 01:50:58 +0900

    update . 저장된 단어장 가져오기

--- prac-memos/src/words/Words.tsx
+++ prac-memos/src/words/Words.tsx
@@ -1,20 +1,35 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import { useWord } from "./useWordQuery";
 import WordsInput from "./WordsInput";
 
 export default function Words() {
   const [toggleState, setToggleState] = useState(false);
+
+  const { data = [] } = useWord.useGetWord();
+  console.log(data);
   return (
-    <Wrap>
-      <button
-        onClick={() => {
-          setToggleState(true);
-        }}
-      >
-        메모 작성하기
-      </button>
-      {toggleState && <WordsInput togle={setToggleState} />}
-    </Wrap>
+    <>
+      <Wrap>
+        <button
+          onClick={() => {
+            setToggleState(true);
+          }}
+        >
+          메모 작성하기
+        </button>
+        {toggleState && <WordsInput togle={setToggleState} />}
+        {data.map((v) => {
+          return (
+            <div style={{ border: "1px solid #000" }} key={v.id}>
+              <p>{v.word}</p>
+              <p>{v.mean}</p>
+              <pre>{v.example}</pre>
+            </div>
+          );
+        })}
+      </Wrap>
+    </>
   );
 }
 const Wrap = styled.div`
--- prac-memos/src/words/useWordQuery.tsx
+++ prac-memos/src/words/useWordQuery.tsx
@@ -1,13 +1,19 @@
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, getDocs } from "firebase/firestore";
 import { db } from "../firebase/firebase";
-import { useMutation } from "react-query";
+import { useMutation, useQuery } from "react-query";
 
 interface data {
   word: string;
   example: string;
   mean: string;
 }
-
+// 우선  ?:를 통해 typeerror는 넘어가보자
+interface wordList {
+  id: string;
+  word?: string;
+  example?: string;
+  mean?: string;
+}
 export const useWord = {
   useSaveWord: () => {
     const fetech = async (payload: data) => {
@@ -21,5 +27,16 @@ export const useWord = {
         alert("저장 실패 ");
       },
     });
+  },
+  useGetWord: () => {
+    const fetech = async () => {
+      const wordsData = await getDocs(collection(db, "words"));
+      const wordsList: wordList[] = [];
+      wordsData.forEach((doc) => {
+        wordsList.push({ id: doc.id, ...doc.data() });
+      });
+      return wordsList;
+    };
+    return useQuery("words", fetech);
   },
 };
